42075a7d93c322b8a171d55b83ba95953ef6d01a
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2023-07-04 23:56:37 -0700

    complete exercises 15-21

--- src/03-hooks/19-use-ref-with-elements.problem.tsx
+++ src/03-hooks/19-use-ref-with-elements.problem.tsx
@@ -1,7 +1,8 @@
 import { useRef } from "react";
+import { string } from "zod";
 
 export const Component = () => {
-  const ref = useRef<HTMLDivElement>();
+  const ref = useRef<HTMLDivElement>(null);
 
   return <div ref={ref} />;
 };
